5d34e764b5a40ea6db1e8b074a4dc3d43213e863
clement6494 <94296944+clement6494@users.noreply.github.com>
2021-12-07 12:26:05 +0100

    Add files via upload

---
+++ projet info/projet info 1/header_fonctions.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_FONCTIONS_H_INCLUDED
+#define HEADER_FONCTIONS_H_INCLUDED
+
+
+void pose(int tab[17][17]);
+#endif // HEADER_FONCTIONS_H_INCLUDED
---
+++ projet info/projet info 1/partie.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "header_function.h"
+
+
+void greeting(){
+printf("bienvenue nouveau joueur");
+}
+
+void partie(int tab[17][17]){
+    int tour=0;
+    int fin_partie=1;
+    int action;
+
+    while (fin_partie!== 0){
+        action=debut_tour(tour);
+        if action==1;
+            pose();
+        if action==2;
+            bouge();
+        if action==3;
+            annule();
+        if action==4;
+            passe();
+        tour+=1;
+        test_fin_partie();
+}
+
+}
+
+
+
+int debut_tour(int d){
+t_joueur tour_de;
+int action;
+    if d%2==0;
+        tour_de= joueur1;
+    if d%2==1;
+        tour_de= joueur2;
+
+    affiche_msg_tour(tour_de.nom);
+    scanf(("%d",&action);
+    if (tour_de.barriere==0){
+        while ((action>1)&&(action<=6)){
+        scanf("%d",&action);}
+    }
+    else{
+    while ((action>=1)&&(action<=6)){
+        scanf("%d",&action);}
+    }
+return (action);
+}
+/*Il faut coder ttes les fonctions
+coder*
+pose()
+annule()
+bouge()
+test_fin_partie()*/
